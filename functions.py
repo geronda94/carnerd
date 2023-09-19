0276d5516288c87b7dd1a0b000fb7ad263874e5f
geronda94 <iiiliev.igor@gmail.com>
2023-09-19 23:35:09 +0300

    1234

--- functions.py
+++ functions.py
@@ -33,12 +33,20 @@ def number_validator(number: str):
 
         else:
             return False
-    if 9 >= len(new_number) >= 10 and new_number[0] != '0':
+        
+        
+    if 9 == len(new_number) and new_number[0] != '0':
         new_number = '0'+new_number
 
-    if len(new_number) > 10 and new_number[0] != '+':
+    elif len(new_number) == 10 and new_number[0] != '0':
         return False
-        
+
+    elif len(new_number) >= 11 and new_number[0] != '+':
+        return False
+    
+    else:
+        return False
+    
     return False if 15 < len(new_number) or len(new_number) < 10 else new_number
 
 
